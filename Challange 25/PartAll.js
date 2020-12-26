dd0a89fffa5c21b9994061bbc5a3a6813e821ab2
NateStar0 <natcon0808@gmail.com>
2020-12-26 11:50:54 +1100

    Done!

--- Challange 25/PartAll.js
+++ Challange 25/PartAll.js
@@ -1,18 +1,29 @@
 var input = 
-``
-
-var example_input =
-``
+`19241437
+17346587`
 
 function part1(data)
 {
+	var dice = data.split("\n");
+	var card = dice[0] * 1;
+	var door = dice[1] * 1;
 	
+	var k = 1;
+	var targ = 1;
+	
+	console.log(dice)
+	
+	while(door !== targ)
+	{
+		targ = (targ * 7) % 20201227
+		k = (k * card) % 20201227
+	}
+	
+	return k;
 }
 
-function part2(data)
-{
-
-}
+//Part 2 returns nothing
+//There isn't anything to solve
 
 console.log("part 1: ", part1(input));
 console.log("part 2: ", part2(input));
