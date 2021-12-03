f0c779130874d84fdc49b2e5efcf9fb66db51fcc
NateStar0 <natcon0808@gmail.com>
2021-12-03 20:21:02 +1100

    Day 3 Completed!

--- 2021/main.js
+++ 2021/main.js
@@ -1,5 +1,5 @@
 var fs = require("fs");
-var day = 3;
+var day = 4;
 var data = []; 
 
 fs.readFile('./Inputs/' + day + '.txt', 'utf8', function(e, d) 
@@ -110,12 +110,96 @@ function run(input)
 		{ // 3
 			part1 : function ()
 			{
-				return "A"
+				var split = input.split("\n");
+				var gam = "";
+				var eps = "";
+				
+				for(var i = 0; i < split.length; i++)
+				{
+					split[i] = split[i].split("");
+				}
+				
+				for(var j = 0; j < split[0].length; j++)
+				{
+					var highs = 0;
+					for(var i = 0; i < split.length; i++)
+					{
+						if(split[i][j] == "1") highs ++;
+					}
+					
+					if(highs > split.length / 2)
+					{
+						gam += "1";
+						eps += "0";
+					}
+					else
+					{
+						gam += "0";
+						eps += "1";
+					}
+				}
+				
+				return parseInt(gam, 2) * parseInt(eps, 2)
 			},
 			
 			part2 : function ()
 			{
-				return "B"
+				var split = input.split("\n");
+				var dup = [[]];
+				var ox = [];
+				var cb = [];
+				
+				for(var i = 0; i < split.length; i++)
+				{
+					split[i] = split[i].split("");
+				}
+				
+				dup = JSON.parse(JSON.stringify(split));
+				
+				for(var j = 0; j < split[0].length; j++)
+				{
+					var highs = 0;
+					var lows = 0;
+					for(var i = 0; i < dup.length; i++)
+					{
+						if(dup[i][j] == "1") highs ++;
+						if(dup[i][j] == "0") lows ++;
+					}
+					
+					var selector = (highs >= lows) ? "1" : "0";
+					
+					for(var i = dup.length - 1; i >= 0; i--)
+					{
+						if(dup[i][j] != selector) dup.splice(i, 1);
+					}
+				}
+				
+				ox = JSON.parse(JSON.stringify(dup)).join().replace(/,/g, "");
+				
+				dup = JSON.parse(JSON.stringify(split));
+				
+				for(var j = 0; j < split[0].length; j++)
+				{
+					var highs = 0;
+					var lows = 0;
+					for(var i = 0; i < dup.length; i++)
+					{
+						if(dup[i][j] == "1") highs ++;
+						if(dup[i][j] == "0") lows ++;
+					}
+					
+					var selector = (lows > highs) ? "1" : "0";
+					if(dup.length == 1) selector = dup[0][j];
+					
+					for(var i = dup.length - 1; i >= 0; i--)
+					{
+						if(dup[i][j] != selector) dup.splice(i, 1);
+					}
+				}
+				
+				cb = JSON.parse(JSON.stringify(dup)).join().replace(/,/g, "");
+				
+				return [parseInt(ox, 2), parseInt(cb, 2), parseInt(ox, 2) * parseInt(cb, 2)];
 			}
 		},
 		
@@ -385,7 +469,13 @@ function run(input)
 	]
 }
 
+
 function lengthdir(l, d)
 {
 	return { x : l * Math.cos(d), y : l * -Math.sin(d) };
+}
+
+function log(a)
+{
+	console.log(a)
 }
