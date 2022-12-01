e2ae1537a6b646c3d1c786fb54d0289eb662a48d
NateStar0 <natcon0808@gmail.com>
2022-12-01 16:23:31 +1100

    better day 1

--- 2022/processes/1.js
+++ 2022/processes/1.js
@@ -5,26 +5,14 @@ module.exports =
     {
         let part1 = (data) =>
         {
-            let elves = data.split("\n\n").map(v => v.split("\n").map(Number));
-
-            for(let i = 0; i < elves.length; i++)
-            {
-                elves[i] = elves[i].reduce((partialSum, a) => partialSum + a, 0);
-            }
-
+            let elves = data.split("\n\n").map(v => v.split("\n").map(Number).reduce((sum, a) => sum + a, 0));
             console.log(elves.sort((a, b) => {return b - a})[0]);
         }
 
         let part2 = (data) =>
         {
-            let elves = data.split("\n\n").map(v => v.split("\n").map(Number));
-
-            for(let i = 0; i < elves.length; i++)
-            {
-                elves[i] = elves[i].reduce((partialSum, a) => partialSum + a, 0);
-            }
-
-            elves.sort((a, b) => {return b - a})
+            let elves = data.split("\n\n").map(v => v.split("\n").map(Number).reduce((sum, a) => sum + a, 0));
+            elves.sort((a, b) => {return b - a});
 
             console.log(elves[0] + elves[1] + elves[2]);
         }
