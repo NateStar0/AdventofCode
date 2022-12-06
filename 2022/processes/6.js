fc5b36e0e98078a6dc49922c901a9957a73017e0
NateStar0 <natcon0808@gmail.com>
2022-12-06 16:43:05 +1100

    Day 6!

--- 2022/processes/6.js
+++ 2022/processes/6.js
@@ -5,12 +5,50 @@ module.exports =
     {
         let part1 = (data) =>
         {
+            for(let i = 3; i < data.length; i++)
+            {
+                let caught = [];
 
+                for(let j = i - 3; j <= i; j++)
+                {
+                    let presence = data[j];
+
+                    if(!caught.includes(presence))
+                    {
+                        caught.push(presence);
+                    }
+                }
+
+                if(caught.length == 4)
+                {
+                    console.log(i + 1);
+                    return;
+                }
+            }
         }
 
         let part2 = (data) =>
         {
+            for(let i = 13; i < data.length; i++)
+            {
+                let caught = [];
 
+                for(let j = i - 13; j <= i; j++)
+                {
+                    let presence = data[j];
+
+                    if(!caught.includes(presence))
+                    {
+                        caught.push(presence);
+                    }
+                }
+
+                if(caught.length == 14)
+                {
+                    console.log(i + 1);
+                    return;
+                }
+            }
         }
         
         part1(data);
