fa84181cce8302cfd6c22dd7f0288d176f2e0c4d
NateStar0 <natcon0808@gmail.com>
2022-12-12 17:22:49 +1100

    day 12

--- 2022/processes/12.js
+++ 2022/processes/12.js
@@ -1,16 +1,109 @@
 
+const dk = require("dijkstrajs")
+
 module.exports = 
 {
     run(data)
     {
+        let letters = "SabcdefghijklmnopqrstuvwxyzE"
+        data = data.split("\n").map(n => n.split("").map(m => letters.indexOf(m)));
+
+        let start = [0, 0];
+        let end = [0, 0];
+
+        for(let i = 0; i < data.length; i++)
+        {
+            for(let j = 0; j < data[i].length; j++)
+            {
+                switch(data[i][j])
+                {
+                    case 0: 
+                        start = [i, j].join(",");
+                    break;
+                    case 27: 
+                        end = [i, j].join(",");
+                    break;
+                    default: break;
+                }
+            }
+        }
+
+        let graph = {};
+
+        for(let i = 0; i < data.length; i++)
+        {
+            for(let j = 0; j < data[i].length; j++)
+            {
+                let links = {};
+
+                if(i > 0)
+                {
+                    if(data[i][j] - data[i - 1][j] >= -1)
+                    {
+                        links[`${i - 1},${j}`] = 0;
+                    }
+                } 
+
+                if(j > 0)
+                {
+                    if((data[i][j] - data[i][j - 1])  >= -1)
+                    {
+                        links[`${i},${j - 1}`] = 0;
+                    }
+                } 
+
+                if(i < data.length - 1)
+                {
+                    if((data[i][j] - data[i + 1][j])  >= -1)
+                    {
+                        links[`${i + 1},${j}`] = 0;
+                    }
+                } 
+
+                if(j < data[0].length - 1)
+                {
+                    if((data[i][j] - data[i][j + 1]) >= -1)
+                    {
+                        links[`${i},${j + 1}`] = 1;
+                    }
+                } 
+
+                graph[`${i},${j}`] = links;
+            }
+        }
+
         let part1 = (data) =>
         {
-            return "";
+            let path = dk.find_path(graph, start, end);
+
+            return path.length - 1;
         }
 
         let part2 = (data) =>
         {
-            return "";
+            let smallest = 9999999;
+
+            for(let i = 0; i < data.length; i++)
+            {
+                for(let j = 0; j < data[i].length; j++)
+                {
+                    if(data[i][j] == letters.indexOf("a"))
+                    {
+                        try 
+                        {
+                            let p = dk.find_path(graph, `${i},${j}`, end).length - 1;
+
+                            smallest = (p < smallest) ? p : smallest;
+                        }
+                        catch
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return smallest;
         }
         
         console.log(part1(data));
