dc62a656a89a3d15cc7ee46a3464fbcccab9577a
NateStar0 <natcon0808@gmail.com>
2022-12-14 17:34:51 +1100

    day 13 / 14!

--- 2022/processes/13.js
+++ 2022/processes/13.js
@@ -1,16 +1,73 @@
 
+
 module.exports = 
 {
     run(data)
     {
+        let compare = (a, b) =>
+        {
+            if (!a && a !== 0) return 1;
+            if (!b && b !== 0) return -1;
+
+            if (Array.isArray(a) || Array.isArray(b)) 
+            {
+                return compArr(Array.isArray(a) ? a : [a], Array.isArray(b) ? b : [b])
+            }
+
+            if (a === b) return 0;
+            return a < b ? 1 : -1;
+        }
+
+        let compArr = (a, b) =>
+        {
+            for (let i = 0; i < Math.max(a.length, b.length); i++) 
+            {
+                let fine = compare(a[i], b[i]);
+
+                if (fine !== 0) return fine;
+            }
+
+            return 0;
+        }
+
+        data = data.split("\n\n").map(n => n.split("\n").map(m => {return JSON.parse(m)}));
+
         let part1 = (data) =>
         {
-            return "";
+            let sum = [];
+
+            for(let n = 0; n < data.length; n++)
+            {
+                let [first, second] = data[n];
+
+                let order = compare(first, second);
+
+                sum.push( (order > -1) ? n + 1 : 0);
+
+            };
+
+            return sum.reduce((acc, i) => acc += i, 0);
         }
 
         let part2 = (data) =>
         {
-            return "";
+            let all = [[[2]], [[6]]];
+            let sum = 1;
+    
+            data.forEach(pair => { all.push(pair[0], pair[1]) });
+            all.sort((a, b) => compare(b, a));
+
+            for(let i = 0; i < all.length; i++)
+            {
+                let e = all[i];
+
+                if(compare(e, [[2]]) === 0 || compare(e, [[6]]) === 0) 
+                {
+                    sum *= (i + 1);
+                }
+            }
+
+            return sum;
         }
         
         console.log(part1(data));
--- 2022/processes/14.js
+++ 2022/processes/14.js
@@ -3,14 +3,162 @@ module.exports =
 {
     run(data)
     {
+        data = data.replace(/ /g, "").split("\n").map(n => n.split("->").map(m => m.split(",").map(Number)));
+
+        class point
+        {
+            constructor (_x = 0, _y = 0)
+            {
+                this.x = _x;
+                this.y = _y;
+            }
+        }
+
         let part1 = (data) =>
         {
-            return "";
+            let lowest = 0;
+
+            let contains = new Set();
+
+            data.forEach(e =>
+            {
+                console.log(e)
+                for(let i = 1; i < e.length; i++)
+                {
+                    let minX = Math.min(e[i - 1][0], e[i][0]);
+                    let maxX = Math.max(e[i - 1][0], e[i][0]);
+                    let minY = Math.min(e[i - 1][1], e[i][1]);
+                    let maxY = Math.max(e[i - 1][1], e[i][1]);
+
+                    for (let y = minY; y <= maxY; y++) 
+                    {
+                        for (let x = minX; x <= maxX; x++) 
+                        {
+                            if(y > lowest) lowest = y;
+
+                            contains.add(`${x},${y}`);
+                        }
+                    }
+                }
+            });
+
+            let sum = 0;
+            let finished = false;
+
+            let currentSand = new point(500, 0);
+
+            while (!finished) 
+            {
+                while (!finished) 
+                {
+                    if (contains.has(`${currentSand.x},${currentSand.y + 1}`)) 
+                    {
+                        if (!contains.has(`${currentSand.x - 1},${currentSand.y + 1}`)) 
+                        {
+                            currentSand.x--;
+                            currentSand.y++;
+                        } 
+                        else 
+                        if (!contains.has(`${currentSand.x + 1},${currentSand.y + 1}`)) 
+                        {
+                            currentSand.x++;
+                            currentSand.y++;
+                        } 
+                        else 
+                        {
+                            contains.add(`${currentSand.x},${currentSand.y}`);
+                            break;
+                        }
+                    } 
+                    else 
+                    {
+                        currentSand.y++;
+                    }
+        
+                    if (currentSand.y > lowest) finished = true;
+                }
+
+                currentSand = new point(500, 0);
+                sum++;
+            }
+
+
+            return sum - 1// grid);
         }
 
         let part2 = (data) =>
         {
-            return "";
+            let lowest = 0;
+
+            let contains = new Set();
+
+            data.forEach(e =>
+            {
+                console.log(e)
+                for(let i = 1; i < e.length; i++)
+                {
+                    let minX = Math.min(e[i - 1][0], e[i][0]);
+                    let maxX = Math.max(e[i - 1][0], e[i][0]);
+                    let minY = Math.min(e[i - 1][1], e[i][1]);
+                    let maxY = Math.max(e[i - 1][1], e[i][1]);
+
+                    for (let y = minY; y <= maxY; y++) 
+                    {
+                        for (let x = minX; x <= maxX; x++) 
+                        {
+                            if(y > lowest) lowest = y;
+
+                            contains.add(`${x},${y}`);
+                        }
+                    }
+                }
+            });
+
+            let sum = 0;
+
+            let currentSand = new point(500, 0);
+
+            while (!contains.has('500,0')) 
+            {
+                while (true) 
+                {
+                    if (contains.has(`${currentSand.x},${currentSand.y + 1}`)) 
+                    {
+                        if (!contains.has(`${currentSand.x - 1},${currentSand.y + 1}`)) 
+                        {
+                            currentSand.x--;
+                            currentSand.y++;
+                        } else 
+                        if (!contains.has(`${currentSand.x + 1},${currentSand.y + 1}`)) 
+                        {
+                            currentSand.x++;
+                            currentSand.y++;
+                        } 
+                        else 
+                        {
+                            contains.add(`${currentSand.x},${currentSand.y}`);
+                            break;
+                        }
+                    } 
+                    else 
+                    {
+                        currentSand.y++;
+                    }
+        
+                    if (currentSand.y == lowest + 1) 
+                    {
+                        contains.add(`${currentSand.x},${currentSand.y}`);
+                        break;
+                    }
+                }
+
+                currentSand = new point(500, 0);
+
+                sum++;
+            }
+
+
+            return sum // grid);
         }
         
         console.log(part1(data));
