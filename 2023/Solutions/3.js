f850a6d021426834184f7ec526a90cc0d23225e5
NateStar0 <natcon0808@gmail.com>
2023-12-04 16:32:16 +1100

    Day 3 + 4!

--- 2023/Solutions/3.js
+++ 2023/Solutions/3.js
@@ -5,12 +5,127 @@ module.exports =
     {
         let part1 = (data) =>
         {
-            return 0
+            let sum = 0;
+            let grid = data.split("\n").map(line => line.split(""));
+            let width = grid[0].length;
+            let height = grid.length;
+
+            let counted = new Array(height);
+            for(let i = 0; i < height; i++) counted[i] = new Array(width).fill(false);
+
+            let isDigit = (char) => !isNaN(char * 1);
+            let isSpecial = (char) => !(isDigit(char) || char == ".");
+            let outOfBounds = (y, x) => (x < 0) || (y < 0) || (x >= width) || (y >= height);
+
+            let getWholeValue = (y, x) =>
+            {
+                let length = x;
+                let start = x;
+
+                while(start >= 0 && isDigit(grid[y][start])) start--;
+                while(length <= width && isDigit(grid[y][length])) length ++;
+
+                let accumulator = ""
+
+                for(let i = start + 1; i <= length - 1; ++i)
+                {
+                    accumulator += grid[y][i];
+                    counted[y][i] = 1;
+                }
+
+                return accumulator * 1
+            }
+
+            for(var x = 0; x < width; x++)
+            {
+                for(let y = 0; y < height; y++)
+                {
+                    if(isSpecial(grid[y][x]))
+                    {
+                        for(let i = -1; i <= 1; i++)
+                        {
+                            for(let j = -1; j <= 1; j++)
+                            {
+                                if(!(i == j && i == 0) && !outOfBounds(y + i, x + j))
+                                {
+                                    if(!counted[y + i][x + j] && isDigit(grid[y + i][x + j]))
+                                    {
+                                        sum += getWholeValue(y + i, x + j)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return sum
         }
 
         let part2 = (data) =>
         {
-            return 0
+            let sum = 0;
+            let grid = data.split("\n").map(line => line.split(""));
+            let width = grid[0].length;
+            let height = grid.length;
+
+            let counted = new Array(height);
+            for(let i = 0; i < height; i++) counted[i] = new Array(width).fill(false);
+
+            let isDigit = (char) => !isNaN(char * 1);
+            let isSpecial = (char) => char == "*";
+            let outOfBounds = (y, x) => (x < 0) || (y < 0) || (x >= width) || (y >= height);
+
+            let getWholeValue = (y, x) =>
+            {
+                let length = x;
+                let start = x;
+
+                while(start >= 0 && isDigit(grid[y][start])) start--;
+                while(length <= width && isDigit(grid[y][length])) length ++;
+
+                let accumulator = ""
+
+                for(let i = start + 1; i <= length - 1; ++i)
+                {
+                    accumulator += grid[y][i];
+                    counted[y][i] = 1;
+                }
+
+                return accumulator * 1
+            }
+
+            for(var x = 0; x < width; x++)
+            {
+                for(let y = 0; y < height; y++)
+                {
+                    if(isSpecial(grid[y][x]))
+                    {
+                        let ratioValues = [];
+
+                        for(let i = -1; i <= 1; i++)
+                        {
+                            for(let j = -1; j <= 1; j++)
+                            {
+                                if(!(i == j && i == 0) && !outOfBounds(y + i, x + j))
+                                {
+                                    if(!counted[y + i][x + j] && isDigit(grid[y + i][x + j]))
+                                    {
+                                        ratioValues.push(getWholeValue(y + i, x + j));
+                                    }
+                                }
+                            }
+                        }
+
+                        if(ratioValues.length == 2)
+                        {
+                            sum += ratioValues.reduce((acc, val) => acc * val, 1);
+                        }
+                    }
+                }
+            }
+
+            return sum
         }
         
         console.log(part1(data));
--- 2023/Solutions/4.js
+++ 2023/Solutions/4.js
@@ -5,12 +5,55 @@ module.exports =
     {
         let part1 = (data) =>
         {
-            return 0
+            let sum = 0;
+            let cards = data.replace(/  /g, " ").split("\n")
+                    .map(card => card.split(": ")[1].split(" | ")
+                        .map(numbers => numbers.split(" ").map(Number)))
+                    .forEach(card =>
+                    {
+                        let tally = 0;
+
+                        card[0].forEach(num =>
+                        {
+                            tally += (card[1].includes(num) ? 1 : 0)
+                        })
+
+                        sum += (tally == 0) ? 0 : Math.pow(2, tally - 1);
+                    })
+            return sum
         }
 
         let part2 = (data) =>
         {
-            return 0
+            let sum = 0;
+            let cards = data.replace(/  /g, " ").split("\n")
+                    .map(card => card.split(": ")[1].split(" | ")
+                        .map(numbers => numbers.split(" ").map(Number)))
+
+            let doCard = (index) =>
+            {
+                let card = cards[index];
+                let tally = 0;
+
+                card[0].forEach(num =>
+                {
+                    tally += (card[1].includes(num) ? 1 : 0)
+                })
+
+                for(let i = 0; i < tally; i++)
+                {
+                    doCard(index + 1 + i);
+                }
+
+                sum++;
+            }
+
+            for(let i = 0; i < cards.length; i++)
+            {
+                doCard(i);
+            }
+
+            return sum
         }
         
         console.log(part1(data));
