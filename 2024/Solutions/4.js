9a2d56ee324582e0b446ce92b6db98da40b16556
NateStar0 <natcon0808@gmail.com>
2024-12-04 18:22:13 +1100

    Day 4

--- 2024/Solutions/4.js
+++ 2024/Solutions/4.js
@@ -5,12 +5,63 @@ module.exports =
     {
         let part1 = (data) =>
         {
-            return 0
+            let sum = 0;
+
+            data = data.split("\n").map(line => line.split(""));
+
+            for(let i = 0; i < data.length; i++)
+            {
+                for(let j = 0; j < data[i].length; j++)
+                {
+                    if(i <= data.length - 4 && ((data[i][j] == "X" && data[i + 1][j] == "M" && data[i + 2][j] == "A" && data[i + 3][j] == "S") || (data[i][j] == "S" && data[i + 1][j] == "A" && data[i + 2][j] == "M" && data[i + 3][j] == "X")))
+                    { 
+                        sum++;
+                    }
+
+                    if(j <= data[i].length - 4 && ((data[i][j] == "X" && data[i][j + 1] == "M" && data[i][j + 2] == "A" && data[i][j + 3] == "S") || (data[i][j] == "S" && data[i][j + 1] == "A" && data[i][j + 2] == "M" && data[i][j + 3] == "X")))
+                    { 
+                        sum++;
+                    }
+
+                    if(j <= data[i].length - 4 && i <= data.length - 4 &&((data[i][j] == "X" && data[i+1][j + 1] == "M" && data[i+2][j + 2] == "A" && data[i+3][j + 3] == "S") || (data[i][j] == "S" && data[i+1][j + 1] == "A" && data[i+2][j + 2] == "M" && data[i+3][j + 3] == "X")))
+                    { 
+                        sum++;
+                    }
+
+                    if(j <= data[i].length - 4 && i >= 3 &&((data[i][j] == "X" && data[i-1][j + 1] == "M" && data[i-2][j + 2] == "A" && data[i-3][j + 3] == "S") || (data[i][j] == "S" && data[i-1][j + 1] == "A" && data[i-2][j + 2] == "M" && data[i-3][j + 3] == "X")))
+                    { 
+                        sum++;
+                    }
+                }
+            }
+
+            return sum
         }
 
         let part2 = (data) =>
         {
-            return 0
+            let sum = 0;
+
+            data = data.split("\n").map(line => line.split(""));
+
+            for(let i = 1; i < data.length - 1; i++)
+            {
+                for(let j = 1; j < data[i].length - 1; j++)
+                {
+                    if(data[i][j] == "A")
+                    {
+                        if( (data[i - 1][j - 1] == "M" && data[i - 1][j + 1] == "M" && data[i + 1][j - 1] == "S" && data[i + 1][j + 1] == "S") || 
+                            (data[i - 1][j - 1] == "S" && data[i - 1][j + 1] == "S" && data[i + 1][j - 1] == "M" && data[i + 1][j + 1] == "M") || 
+                            (data[i - 1][j - 1] == "S" && data[i - 1][j + 1] == "M" && data[i + 1][j - 1] == "S" && data[i + 1][j + 1] == "M") || 
+                            (data[i - 1][j - 1] == "M" && data[i - 1][j + 1] == "S" && data[i + 1][j - 1] == "M" && data[i + 1][j + 1] == "S"))
+                            {
+                                sum++;
+                            }
+                    }
+                }
+            }
+
+            return sum
         }
         
         console.log(part1(data));
