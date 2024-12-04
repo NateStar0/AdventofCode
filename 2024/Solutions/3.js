accbe30eb73bbf052569ead2d7ad697492f20950
NateStar0 <natcon0808@gmail.com>
2024-12-04 13:23:00 +1100

    Day 3

--- 2024/Solutions/3.js
+++ 2024/Solutions/3.js
@@ -5,12 +5,41 @@ module.exports =
     {
         let part1 = (data) =>
         {
-            return 0
+            let matches = data.match(/mul\(\s*([-+]?\d+(\.\d+)?),\s*([-+]?\d+(\.\d+)?)\s*\)/g);
+            let sum = 0;
+
+            matches.forEach(mulOp => 
+                {
+                    let values = mulOp.match(/[-+]?\d*\.?\d+/g).map(Number);
+                    sum += values[0] * values[1]
+                })
+
+            return sum
         }
 
         let part2 = (data) =>
         {
-            return 0
+            let matches = data.match(/(mul\(\s*([-+]?\d+(\.\d+)?),\s*([-+]?\d+(\.\d+)?)\s*\))|(do\(\))|(don't\(\))/g);
+            let sum = 0;
+            let enabled = true;
+
+            matches.forEach(mulOp => 
+                {
+                    switch(mulOp.split("(")[0])
+                    {   
+                        case "mul": 
+                            if(enabled)
+                            {
+                                let values = mulOp.match(/[-+]?\d*\.?\d+/g).map(Number);
+                                sum += values[0] * values[1]
+                            }
+                        break;
+                        case "do": enabled = true; break;
+                        case "don't": enabled = false; break;
+                    }
+                })
+                
+            return sum
         }
         
         console.log(part1(data));
