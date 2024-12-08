be17a7ae62dc33fb8026088e29cab062f055d8b6
NateStar0 <natcon0808@gmail.com>
2024-12-08 13:43:32 +1100

    Day 7

--- 2024/Solutions/6.js
+++ 2024/Solutions/6.js
@@ -5,12 +5,155 @@ module.exports =
     {
         let part1 = (data) =>
         {
-            return 0
+            data = data.split("\n").map(line => line.split(""));
+
+            let guardPosition = { x : -1, y : -1};
+            let visitGrid = new Array(data.length);
+            let dir = 0;
+
+            for(var i = 0; i < data.length; i++)
+            {
+                visitGrid[i] = new Array(data[i].length);
+                visitGrid[i].fill(false);
+
+                for(var j = 0; j < data[i].length; j++)
+                {
+                    if(data[i][j] == "^")
+                    {
+                        guardPosition = {x:j, y:i};
+                        data[i][j] = ".";
+                    }
+                }
+            }
+
+            let done = false;
+
+            while(!done)
+            {
+                let offsets = 
+                [
+                    {x:0, y:-1, dirdir:"up"},
+                    {x:1, y:0, dirdir:"right"},
+                    {x:0, y:1, dirdir:"down"},
+                    {x:-1, y:0, dirdir:"left"}
+                ]
+
+                if(guardPosition.y + offsets[dir].y > -1 && guardPosition.y + offsets[dir].y < data.length && guardPosition.x + offsets[dir].x > -1 && guardPosition.x + offsets[dir].x < data[0].length)
+                {
+                    visitGrid[guardPosition.y][guardPosition.x] = true;
+                    if(data[guardPosition.y + offsets[dir].y][guardPosition.x + offsets[dir].x] == "#")
+                    {
+                        dir++;
+                        if(dir == offsets.length) dir = 0;
+                    }
+                    else
+                    {
+                        guardPosition.x += offsets[dir].x;
+                        guardPosition.y += offsets[dir].y;
+                    }
+                }
+                else
+                {
+                    done = true;
+                }
+            }
+
+            let sum = 0;
+
+            for(var i = 0; i < data.length; i++)
+            {
+                for(var j = 0; j < data[i].length; j++)
+                {
+                    if(visitGrid[i][j])
+                    {
+                        sum++
+                    }
+                }
+            }
+
+            return (sum + 1)
         }
 
         let part2 = (data) =>
         {
-            return 0
+            data = data.split("\n").map(line => line.split(""));
+
+            let guardPosition = { x : -1, y : -1};
+            let visitGrid = new Array(data.length);
+            let dir = 0;
+
+            for(var i = 0; i < data.length; i++)
+            {
+                visitGrid[i] = new Array(data[i].length);
+                visitGrid[i].fill(false);
+
+                for(var j = 0; j < data[i].length; j++)
+                {
+                    if(data[i][j] == "^")
+                    {
+                        guardPosition = {x:j, y:i};
+                        data[i][j] = ".";
+                    }
+                }
+            }
+
+            let done = false;
+            let changes = 0;
+
+            while(!done)
+            {
+                let offsets = 
+                [
+                    {x:0, y:-1, dirdir:"up"},
+                    {x:1, y:0, dirdir:"right"},
+                    {x:0, y:1, dirdir:"down"},
+                    {x:-1, y:0, dirdir:"left"}
+                ]
+
+                if(guardPosition.y + offsets[dir].y > -1 && guardPosition.y + offsets[dir].y < data.length && guardPosition.x + offsets[dir].x > -1 && guardPosition.x + offsets[dir].x < data[0].length)
+                {
+                    visitGrid[guardPosition.y][guardPosition.x] = true;
+                    if(data[guardPosition.y + offsets[dir].y][guardPosition.x + offsets[dir].x] == "#")
+                    {
+                        dir++;
+                        if(dir == offsets.length) dir = 0;
+
+                        changes++;
+                    }
+                    else
+                    {
+                        guardPosition.x += offsets[dir].x;
+                        guardPosition.y += offsets[dir].y;
+                    }
+                }
+                else
+                {
+                    done = true;
+                }
+            }
+
+            let sum = 0;
+            let visited = '';
+
+            for(var i = 0; i < data.length; i++)
+            {
+                for(var j = 0; j < data[i].length; j++)
+                {
+                    if(visitGrid[i][j])
+                    {
+                        sum++
+                        visited += 'X';
+                    }
+                    else
+                    {
+                        visited += data[i][j];
+                    }
+                }
+
+                visited += '\n'
+            }
+
+            return changes - 2;
         }
         
         console.log(part1(data));
--- 2024/Solutions/7.js
+++ 2024/Solutions/7.js
@@ -5,12 +5,98 @@ module.exports =
     {
         let part1 = (data) =>
         {
-            return 0
+            let sum = 0;
+            data = data.split("\n").map(line => line.split(": ").map(group => group.split(" ").map(Number)))
+            data.forEach(operation =>
+            {
+                let [product, reactants] = operation;
+                product = product[0];
+
+                let operations = Math.pow(2, reactants.length - 1) - 1;
+                let doAdd = false;
+
+                for(let i = 0; i <= operations; i++)
+                {
+                    let opList = (i.toString(2).padStart(operations.toString(2).length, 0)).split("");
+                    let acc = reactants[0]
+                    for(let j = 0; j < opList.length; j++)
+                    {
+                        if(opList[j] == "1")
+                        {
+                            acc = acc * reactants[j + 1];
+                        }
+                        else
+                        {
+                            acc = acc + reactants[j + 1];
+                        }
+                    }
+
+                    if(acc == product)
+                    {
+                        i = operations;
+                        doAdd = true;
+                    }          
+                }
+
+                if(doAdd)
+                {
+                    sum += product;
+                }
+            }
+            )
+
+            return sum
         }
 
         let part2 = (data) =>
         {
-            return 0
+            let sum = 0;
+            data = data.split("\n").map(line => line.split(": ").map(group => group.split(" ").map(Number)))
+            data.forEach(operation =>
+            {
+                let [product, reactants] = operation;
+                product = product[0];
+
+                let operations = Math.pow(3, reactants.length - 1) - 1;
+                let doAdd = false;
+
+                for(let i = 0; i <= operations; i++)
+                {
+                    let opList = (i.toString(3).padStart(operations.toString(3).length, 0)).split("");
+                    let acc = reactants[0]
+                    for(let j = 0; j < opList.length; j++)
+                    {
+                        switch(opList[j])
+                        {
+                            case "0":
+                                acc = acc * reactants[j + 1];
+                            break;
+
+                            case "1":
+                                acc = acc + reactants[j + 1];
+                            break;
+
+                            case "2":
+                                acc = (String(acc) + reactants[j + 1]) * 1;
+                            break;
+                        }
+                    }
+
+                    if(acc == product)
+                    {
+                        i = operations;
+                        doAdd = true;
+                    }          
+                }
+
+                if(doAdd)
+                {
+                    sum += product;
+                }
+            }
+            )
+
+            return sum
         }
         
         console.log(part1(data));
