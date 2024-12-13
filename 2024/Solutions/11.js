28cb3f1ead85c2d24ec08767509daf0db75b9c84
NateStar0 <natcon0808@gmail.com>
2024-12-13 20:41:06 +1100

    Day 13

--- 2024/Solutions/11.js
+++ 2024/Solutions/11.js
@@ -5,12 +5,107 @@ module.exports =
     {
         let part1 = (data) =>
         {
-            return 0
+            data = data.split(" ");
+
+            for(let blinks = 0; blinks < 25; blinks++)
+            {
+                let newStones = [];
+
+                for(var i = 0; i < data.length; i++)
+                {
+                    if(data[i] == "0")
+                    {
+                        newStones.push("1");
+                    } 
+                    else if(data[i].length % 2 == 0)
+                    {
+                        newStones.push(data[i].slice(0, data[i].length / 2));
+                        newStones.push(String(parseInt(data[i].slice(data[i].length / 2, data[i].length), 10)));
+                    }
+                    else
+                    {
+                        newStones.push(String(data[i] * 2024));
+                    }
+                }
+
+                data = newStones
+            }
+
+            return data.length
         }
 
         let part2 = (data) =>
         {
-            return 0
+            data = data.split(" ").map(Number);
+
+            let cache = new Map();
+
+            let parse = (blink, currentList) => 
+            {
+                if (blink === 0) return 1;
+            
+                blink -= 1;
+
+                let len = 0;
+
+                for (let value of currentList) 
+                {
+                    if (value == 0) 
+                    {
+                        if (cache.has(`${blink}:${1}`))
+                        {
+                            len += cache.get(`${blink}:${1}`);
+                        }
+                        else 
+                        {
+                            let sublen = parse(blink, [1]);
+                            cache.set(`${blink}:${1}`, sublen);
+                            len += sublen;
+                        }
+                        continue;
+                    }
+                    
+                    value = String(value)
+                    if (value.length % 2 == 0) 
+                    {
+                        let left = 1 * value.slice(0, value.length / 2);
+                        let right = 1 * value.slice(value.length / 2, value.length);
+                
+                        if (cache.has(`${blink}:${left}`)) len += cache.get(`${blink}:${left}`);
+                        else 
+                        {
+                            let sublen = parse(blink, [left]);
+                            cache.set(`${blink}:${left}`, sublen);
+                            len += sublen;
+                        }
+                
+                        if (cache.has(`${blink}:${right}`)) len += cache.get(`${blink}:${right}`);
+                        else 
+                        {
+                            let sublen = parse(blink, [right]);
+                            cache.set(`${blink}:${right}`, sublen);
+                            len += sublen;
+                        }
+                        continue;
+                    }
+            
+                    let newValue = value * 2024;
+                    if (cache.has(`${blink}:${newValue}`))
+                    {
+                        len += cache.get(`${blink}:${newValue}`);
+                    }
+                    else 
+                    {
+                        let sublen = parse(blink, [newValue]);
+                        cache.set(`${blink}:${newValue}`, sublen);
+                        len += sublen;
+                    }
+                }
+
+                return len;
+            };
+            
+            return parse(75, data);
         }
         
         console.log(part1(data));
--- 2024/Solutions/13.js
+++ 2024/Solutions/13.js
@@ -5,12 +5,75 @@ module.exports =
     {
         let part1 = (data) =>
         {
-            return 0
+            data = data.split("\n\n").map(machine =>
+            {
+                let [a, b, goal] = machine.split("\n").map(line => line.match(/-?\d+(\.\d+)?/g));
+
+                return {a : a, b : b, goal : goal}
+            })
+
+            let sum = 0;
+
+            data.forEach(machine =>
+            {
+                let {a, b, goal} = machine;
+                // goal[0] = A * a[0] + B * b[0]
+                // goal[1] = A * a[1] + B * b[1]
+
+                // Mult 0 by a[1], mult 1 by a[0]
+                // goal[0] * a[1] = A * a[0] * a[1] + B * b[0] * a[1]
+                // goal[1] * a[0] = A * a[1] * a[0] + B * b[1] * a[0]
+
+                // goal[0] * a[1] - goal[1] * a[0] / (b[0] * a[1] - b[1] * a[0]) = B;
+
+                let B = (goal[0] * a[1] - goal[1] * a[0]) / (b[0] * a[1] - b[1] * a[0])
+                let A = (goal[0] - (B * b[0])) / a[0];
+
+                let total = B + 3 * A;
+
+                if(Number.isInteger(total) && A <= 100 && B <= 100)
+                {
+                    sum += total
+                }
+
+                console.log(machine, A, B, 3 * B, total)
+            })
+
+            return sum
         }
 
         let part2 = (data) =>
         {
-            return 0
+            data = data.split("\n\n").map(machine =>
+                {
+                    let [a, b, goal] = machine.split("\n").map(line => line.match(/-?\d+(\.\d+)?/g).map(Number));
+    
+                    goal[0] += 10000000000000;
+                    goal[1] += 10000000000000;
+    
+                    return {a : a, b : b, goal : goal}
+                })
+    
+                let sum = 0;
+    
+                data.forEach(machine =>
+                {
+                    let {a, b, goal} = machine;
+    
+                    let B = (goal[0] * a[1] - goal[1] * a[0]) / (b[0] * a[1] - b[1] * a[0])
+                    let A = (goal[0] - (B * b[0])) / a[0];
+    
+                    let total = B + 3 * A;
+    
+                    if(Number.isInteger(total))
+                    {
+                        sum += total
+                    }
+    
+                    console.log(machine, A, B, 3 * B, total)
+                })
+    
+                return sum
         }
         
         console.log(part1(data));
